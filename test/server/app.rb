80f5373c88c94c3f1ed800a2bed8d822aebdc68f
Florian Nitschmann <florian.nitschmann@infopark.de>
2017-02-14 12:55:59 +0100

    updated test server to new bima-doorkeeper-core gem

--- test/server/app.rb
+++ test/server/app.rb
@@ -1,16 +1,17 @@
 require "bima-http"
-require "bima-doorkeeper-client"
+require "bima-doorkeeper-core"
 require "json"
 require "sinatra/base"
 require "sinatra/reloader"
 require "pry"
 
+# ATTENTSION: These credentials need to be present in config/api_apps.yml in the
+# bima-notifications app
 APP_NAME = "notifications_test_app"
 ACCESS_ID = "notifications_test_app"
 SECRET_KEY = "wHYTfmXpeaRUYOS92aDIhOFnEPzbJ7p5eC3DW1h5/VFkmNTnCHGCrjtT1ToaHmYgz/C5ieiLeM8xVIaXIQExhw=="
 
 class App < Sinatra::Application
-  # enable :sessions
   use Rack::Session::Pool, :expire_after => 2592000
 
   before "/*" do
@@ -18,21 +19,26 @@ class App < Sinatra::Application
       config.apps = {
         "doorkeeper" => {
           "url" => "http://localhost:3000",
-          "access_id" => "dk",
-          "secret_key" => "secret"
+          "access_id" => "doorkeeper",
+          "secret_key" => "doorkeeper"
         }
       }
 
       config.apps[APP_NAME] = {
-        "url" => "http://localhost:3000",
+        "url" => "http://localhost:5000",
         "access_id" => ACCESS_ID,
         "secret_key" => SECRET_KEY
       }
     end
 
-    Doorkeeper.configure { |config| config.client_app_name = APP_NAME }
+    Doorkeeper.configure do |config|
+      config.client_app_name = APP_NAME
+      config.site = BimaHttp.apps["doorkeeper"]["url"]
+    end
   end
 
+  # TODO: This is a really bad hack, but Sinatra seems a  bit buggy here. Maybe
+  # there is another way to get this working...
   get "/Users/*" do
     file_subpath = params["splat"].first
     filepath = "/Users/" + file_subpath
@@ -96,11 +102,8 @@ class App < Sinatra::Application
 
   def get_session_token
     authorization_token = params["client_authorization_token"]
-    path = "/api/sessions/session_token"
-    url = File.join(BimaHttp.apps["doorkeeper"]["url"], path)
-
     response = BimaHttp.post(
-      url,
+      Doorkeeper.config.endpoints[:session_token],
       {
         :params => { :client_authorization_token => authorization_token },
         :headers => { :accept => :json },
@@ -115,45 +118,21 @@ class App < Sinatra::Application
 
   def get_service_token
     if session[:session_token]
-      session_token = session[:session_token]
-      path = "/api/sessions/service_token"
-      url = File.join(BimaHttp.apps["doorkeeper"]["url"], path)
-
-      response = BimaHttp.post(
-        url,
-        {
-          :params => { :session_token => session_token },
-          :headers => { :accept => :json },
-          :app_name => APP_NAME
-        }
-      )
-
-      JSON.parse(response.body)["data"]["attributes"]["jwt"]
+      Doorkeeper::Token.create_service_token(session[:session_token]).jwt
     end
   rescue => e
     nil
   end
 
   def get_session_user_id
-    service_token = session[:service_token]
-    path = "/api/sessions/user"
-    url = File.join(BimaHttp.apps["doorkeeper"]["url"], path)
-
-    response = BimaHttp.get(
-      url,
-      {
-        :params => { :service_token => service_token },
-        :headers => { :accept => :json },
-        :app_name => APP_NAME
-      }
-    )
-
-    JSON.parse(response.body)["data"]["id"]
+    Doorkeeper::User.
+      authenticate_user(:service_token => session[:service_token]).
+      id
   end
 
   def js_sdk_file
     path = "../../dist/#{js_sdk_version}/bima-notifications-sdk.js"
-    File.expand_path(path ,File.dirname(__FILE__))
+    File.expand_path(path, File.dirname(__FILE__))
   end
 
   def js_sdk_version
