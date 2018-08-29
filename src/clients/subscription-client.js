f76a307eca3abe34b07dfc27f70e1523f077c1f9
Grzegorz Jakubiak <grzegorz.jakubiak@infopark.de>
2018-08-29 11:38:05 +0200

    Implement SubscriptionClient

---
+++ src/clients/subscription-client.js
@@ -0,0 +1,42 @@
+'use strict'
+
+const Client = require('./base-client')
+
+class SubscriptionClient {
+  constructor (url) {
+    this.client = new Client({
+      name: 'SubscriptionClient',
+      url: `${url}/subscriptions`,
+      contentType: 'application/vnd.api+json'
+    })
+  }
+
+  search (parameters = {}) {
+    return this.client.search(parameters)
+  }
+
+  create (data, parameters = {}) {
+    return this.client.create(data, parameters)
+  }
+
+  destroy (id, parameters = {}) {
+    return this.client.destroy(id, parameters)
+  }
+
+  bulkCreate (data) {
+    return this.__bulkOperation(`${this.client.baseUrl}/bulk_creation`, data)
+  }
+
+  bulkDelete (data) {
+    return this.__bulkOperation(`${this.client.baseUrl}/bulk_deletion`, data)
+  }
+
+  __bulkOperation (url, data) {
+    return this.client.sendRequest(url, {
+      body: data,
+      method: 'POST'
+    })
+  }
+}
+
+module.exports = SubscriptionClient
