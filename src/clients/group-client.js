c7dc2a309aefd5124c4162f39c30a68cbf330be1
Joergen Dahlke <joergen.dahlke@infopark.de>
2018-10-12 11:56:16 +0200

    add group client

---
+++ src/clients/group-client.js
@@ -0,0 +1,35 @@
+'use strict'
+
+const Client = require('./base-client')
+
+class GroupClient {
+  constructor (url) {
+    this.client = new Client({
+      name: 'GroupClient',
+      url: `${url}/api/groups`,
+      contentType: 'application/vnd.api+json'
+    })
+  }
+
+  search (parameters = {}) {
+    return this.client.search(parameters)
+  }
+
+  find (id, parameters = {}) {
+    return this.client.find(id, parameters)
+  }
+
+  create (data, parameters = {}) {
+    return this.client.create(data, parameters)
+  }
+
+  update (id, data, parameters = {}) {
+    return this.client.patch(id, data, parameters)
+  }
+
+  destroy (id, parameters = {}) {
+    return this.client.destroy(id, parameters)
+  }
+}
+
+module.exports = GroupClient
